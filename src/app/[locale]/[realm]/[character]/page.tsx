4781c3a11ef252a03154be3f71e8e0fc38494974
Rauraurautis <rautanokka@gmail.com>
2023-10-23 02:58:33 +0300

    changes

--- src/app/[locale]/[realm]/[character]/page.tsx
+++ src/app/[locale]/[realm]/[character]/page.tsx
@@ -5,6 +5,7 @@ import SearchWithAutocomplete from "@/app/_components/characters/SearchWithAutoc
 import { setAccessToken } from "@/app/_lib/firebase"
 import { getCharacterFromFile, readFromFile } from "@/app/_lib/utils/serverFunctions"
 import { ToastContainer } from "react-toastify"
+import Image from "next/image"
 
 export default async function Home({ params }: { params: { locale: string, realm: string, character: string } }) {
     const characterData = readFromFile()
@@ -14,12 +15,12 @@ export default async function Home({ params }: { params: { locale: string, realm
         <>
             <div className="absolute right-0 top-0 p-3 w-[60px] hover:animate-wiggle hidden sm:flex ">
                 <a href={"https://github.com/Rauraurautis"} target="_blank">
-                    <img src="/github.png" alt="Github" />
+                    <Image src="/github.png" alt="Github" width={30} height={30} />
                 </a>
             </div>
             <ToastContainer position="top-left" />
             <div className="flex flex-col items-center justify-center xl:flex-row xl:items-start xl:justify-between w-[100%] xl:w-[60%] h-[100%] ">
-                {characterData && <SearchWithAutocomplete allCharacters={characterData} /> }
+                {characterData && <SearchWithAutocomplete allCharacters={characterData} />}
                 <CharacterInfo character={character} />
             </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import SearchWithAutocomplete from "./_components/characters/SearchWithAutocompl
 import { readFromFile } from "./_lib/utils/serverFunctions"
 import { setAccessToken } from "./_lib/firebase"
 import { ToastContainer } from "react-toastify"
+import Image from "next/image"
 
 export default async function Home() {
     const characterData = readFromFile()
@@ -11,7 +12,7 @@ export default async function Home() {
         <>
             <div className="absolute right-0 top-0 p-3 w-[60px] hover:animate-wiggle hidden sm:flex ">
                 <a href={"https://github.com/Rauraurautis"} target="_blank">
-                    <img src="/github.png" alt="Github" />
+                    <Image src="/github.png" alt="Github" width={30} height={30} />
                 </a>
             </div>
             <ToastContainer position="top-left" />
